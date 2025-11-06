ea9a44de36940317daec3f98eb74261425e47ea1
RAJ <razkumarji27@gmail.com>
2025-11-06 20:50:21 +0530

    Update Recommend.jsx

--- src/pages/Recommend.jsx
+++ src/pages/Recommend.jsx
@@ -3,7 +3,7 @@ import Cards from '../components/Cards'
 
 const Recommend = () => {
   return (
-    <div className='w-full h-screen bg-[#121927] mt-16 '>
+    <div className='w-full h-screen bg-[#121927] mt-3 '>
       <div className='text-6xl leading-none tracking-tight text-center bg-linear-to-b from-white to-sky-400 bg-clip-text text-transparent  '>
         <h1>Recommended For You</h1>
       </div>
